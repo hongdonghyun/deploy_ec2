ae2358a60af7b410eccb9712bf999dbebaa4c7c0
hongdonghyun <hong4367@gmail.com>
2017-07-02 16:50:06 +0900

    static_root 설정 ignore제외

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -2,9 +2,10 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEBUG = False
 ALLOWED_HOSTS = config_secret_deploy["django"]["allowed_hosts"]
 
-
-print('@@@@@@ DEBUG:',DEBUG)
-print('@@@@@@ ALLOWED_HOSTS:',ALLOWED_HOSTS)
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
