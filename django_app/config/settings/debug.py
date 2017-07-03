20589a9a70affd5086d30f8f486b72ea98ca147e
hongdonghyun <hong4367@gmail.com>
2017-07-03 12:10:09 +0900

    wsgi분할 취소 url오류 수정 debug,deploy WSGI_APPLICATION추가

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -2,6 +2,8 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
+WSGI_APPLICATION = 'config.wsgi.debug.application'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEBUG = True
